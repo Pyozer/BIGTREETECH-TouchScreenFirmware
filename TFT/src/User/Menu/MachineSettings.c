18fcffcfe5cbd458300ede954fd4ba2680324cce
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2020-05-21 12:30:50 +0200

    Fix PID Autotune

--- TFT/src/User/Menu/MachineSettings.c
+++ TFT/src/User/Menu/MachineSettings.c
@@ -239,15 +239,10 @@ void menuMachineSettings(void)
       case KEY_ICON_4:
         storeCmd("M106 P0 S255\n"); // Turn on fan 100%
         storeCmd("M303 E0 S205 C8 U1\n"); // Start PID autotune hotend
-        popupPauseForUser();
-        storeCmd("M107\n"); // Turn off fan
-        storeCmd("M500\n"); // Save
         break;
 
       case KEY_ICON_5:
         storeCmd("M303 E-1 S60 C8 U1\n"); // Start PID autotune bed
-        popupPauseForUser();
-        storeCmd("M500\n"); // Save
         break;
 
       case KEY_ICON_7:
